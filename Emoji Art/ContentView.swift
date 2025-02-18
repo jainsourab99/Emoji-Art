76edac1d95d5f93fbc6c9d00540a10322071172d
Sourabh Jain <jainsourab.99@gmail.com>
2025-02-18 10:12:30 +0530

    Initial Commit

---
+++ Emoji Art/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Emoji Art
+//
+//  Created by Sourabh Jain on 18/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
